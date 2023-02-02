b5202cab37122467df6df24646a8cc0e430e05d4
Ahmad Mujahid <ahmad.kirghizia@gmail.com>
2023-02-02 23:55:09 +0700

    build(fetchapp): enabling cache
    add cache dependency in build.gradle.kts
    add @EnableCaching in main class

--- fetchapp/build.gradle.kts
+++ fetchapp/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-cache:3.0.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- fetchapp/src/main/kotlin/com/efishery/fetchapp/FetchappApplication.kt
+++ fetchapp/src/main/kotlin/com/efishery/fetchapp/FetchappApplication.kt
@@ -2,10 +2,12 @@ package com.efishery.fetchapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
 @EnableWebFlux
+@EnableCaching
 class FetchappApplication
 
 fun main(args: Array<String>) {
